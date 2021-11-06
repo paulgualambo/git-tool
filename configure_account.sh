bc8f086bb9e7e0f17af2acef35405d51c4f29419
Paul Gualambo <paul.gualambo@gmail.com>
2021-11-06 01:22:14 -0500

    bugfix/GGERP-1-git-tool (add file configure_account)

---
+++ configure_account.sh
@@ -0,0 +1,9 @@
+#Global
+git config --global user.email paul.gualambo@gmail.com
+git config --global user.name 'paul gualambo giraldo'
+
+
+#Proyecto
+git config user.email paul.gualambo@gmail.com
+git config user.name 'paul gualambo giraldo'
+
